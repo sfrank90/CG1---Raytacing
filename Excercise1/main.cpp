37c31bccc4c6571837e81ac656095870b469c448
Sven Frank <sven.frank90@gmail.com>
2013-11-08 16:58:11 +0100

    Add 3 calculations for normal of triangle

--- Excercise1/main.cpp
+++ Excercise1/main.cpp
@@ -8,7 +8,7 @@ std::ostream& operator<<(std::ostream& ostr, cg1::Vec3f const& v)  {
 }
 
 int main() {
-	float pArray[] = {2.0f,2.0f,0.0f};
+	/*float pArray[] = {2.0f,2.0f,0.0f};
 
 	cg1::Vec3f v1(1.0f),
 		       v2(0.0f, -1.0f, 0.0f),
@@ -27,7 +27,7 @@ int main() {
 
 	std::cout << "v1,v2,v3 are defining a triangle t. " << std::endl;
 	cg1::Triangle t(v1,v2,v3);
-	std::cout << "Normal vector of t: " << t.getNormal() << std::endl;
+	std::cout << "Normal vector of t: " << t.getNormal() << std::endl;*/
 
 	//Test section for comparision with written calculations
 	std::cout << "\nTest section for comparision with written calculations" << std::endl;
@@ -71,6 +71,24 @@ int main() {
 	std::cout << "a " << vecDot1a << " + b " << vecDot1b << " = " << vecDot1a*vecDot1b << std::endl;
 	std::cout << "a " << vecDot2a << " + b " << vecDot2b << " = " << vecDot2a*vecDot2b << std::endl;
 	std::cout << "a " << vecDot3a << " + b " << vecDot3b << " = " << vecDot3a*vecDot3b << std::endl;
+
+	//Test section normal of a triangle
+	std::cout << "\nTest section normal of a triangle:" << std::endl;
+
+	cg1::Vec3f vec1_0(3.0f,3.0f,1.0f), vec1_1(1.0f,7.0f,4.0f), vec1_2(5.0f,4.0f,1.0f);
+	cg1::Triangle t1(vec1_0,vec1_1,vec1_2);
+	std::cout << "Vectors of triangle t1: v0 " << vec1_0 << ", v1 " << vec1_1 << ", v2 " << vec1_2 << std::endl;
+	std::cout << "Normal vector of t1: " << t1.getNormal() << std::endl;
+
+	cg1::Vec3f vec2_0(10.0f,3.0f,1.0f), vec2_1(7.0f,5.0f,-3.0f), vec2_2(1.0f,2.0f,5.0f);
+	cg1::Triangle t2(vec2_0,vec2_1,vec2_2);
+	std::cout << "Vectors of triangle t2: v0 " << vec2_0 << ", v1 " << vec2_1 << ", v2 " << vec2_2 << std::endl;
+	std::cout << "Normal vector of t2: " << t2.getNormal() << std::endl;
+
+	cg1::Vec3f vec3_0(7.0f,5.0f,6.0f), vec3_1(13.0f,14.0f,3.0f), vec3_2(17.0f,7.0f,4.0f);
+	cg1::Triangle t3(vec3_0,vec3_1,vec3_2);
+	std::cout << "Vectors of triangle t3: v0 " << vec3_0 << ", v1 " << vec3_1 << ", v3 " << vec3_2 << std::endl;
+	std::cout << "Normal vector of t3: " << t3.getNormal() << std::endl;
 
 	std::cin.get();
 	return 0;
