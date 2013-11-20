cbdfa325fb3f79ce2213fc77a0899c630f99c8d3
Sven Frank <izcip04.ibr.cs.tu-bs.de>
2013-11-20 12:06:59 +0100

    added implementation for 2.1

--- Excercise2/microTrace02/include/02/Primitive02.h
+++ Excercise2/microTrace02/include/02/Primitive02.h
@@ -39,14 +39,26 @@ public:
   {
     // IMPLEMENT ME 2.1
     // (1) Generate a secondary ray. Set the ray origin to the surface point
-    //     that has been hit, but do not set direction quite yet.
+    //     that has been hit, but do not set direction quite yet.   
+    Ray r2 = new Ray();
+    r2.org = ray.org + ray.dir * ray.t;
+
     // (2) Call the illuminate method of your light source, store the intensity.
+    Vec3f intensity = (0,0,0);
+    light->illuminate(r2, intensity);
+
     // (3) Check whether the angle between surface normal and direction to
     //     light source is <= 90 deg (the surface is lit).
     //     If so, modulate the stored intensity with the cosine of the angle
     //     between surface normal and direction to light source.
-    // IMPLEMENT ME END
+    Vec3f s_normal = r2.hit->getNormal(r2);
+    float cos_angle = dot(s_normal, r2.dir)/(norm(s_normal)*norm(r2.dir));
+    if(cos_angle <= 0)
+        intensity = intensity * cos_angle;
+    else
+        intensity = (0,0,0);
 
+    // IMPLEMENT ME END
     return intensity;
   }
 
