6fd3b98d2e3639cc23cf1df67afffcedf2e87cad
Alexander Knueppel <aknueppe@uos.de>
2013-12-08 10:57:55 +0100

    Corrected MirrorShader

--- Excercise3/microTrace03/include/03/MirrorShader.h
+++ Excercise3/microTrace03/include/03/MirrorShader.h
@@ -24,23 +24,27 @@ public:
   {
     if (++ray.recursionLevel < MAX_RECURSION_LEVEL)
     {
-      Vec3f result;
+      Vec3f result, rv;
 
       // IMPLEMENT ME 3.1
       // (1) Get normal of the hit surface.
 	  Vec3f s_normal = ray.hit->getNormal(ray);
 
       // (2) Compute reflection vector.
-	  reflection = 2 * (dot(s_normal, ray.dir)) * s_normal - ray.dir;
+
+	  rv = 2 * (dot(s_normal, ray.dir)) * s_normal - ray.dir;
+	  normalize(rv);
 
       // (3) Change ray direction and position.
 	  ray.org = ray.org + ray.dir * ray.t;
-	  ray.dir = -reflection;
+	  ray.dir = -rv;    //
+	  ray.t = INFINITY; // 
+	  ray.hit = NULL;   // Nothing hitted yet
 	  
 	  //PROBLEM: Output Bild 1 für den Shader schneidet die Spiegelungen auf dem Boden ab?!
 
       // (4) Trace the ray back into the scene.
-	  result = scene->rayTrace(ray);
+	  result = componentProduct(reflection,scene->rayTrace(ray)); // "reflection" controls, what should be exactly reflected, (1,1,1) means ideal reflection like a mirror
       // IMPLEMENT ME END
 
       return result;
