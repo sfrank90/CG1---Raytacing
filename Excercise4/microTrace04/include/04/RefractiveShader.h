11ddb8999def23136fa8b98d13f171af41d66215
Alexander Knueppel <aknueppe@uos.de>
2013-12-15 16:13:05 +0100

    Refraction done

--- Excercise4/microTrace04/include/04/RefractiveShader.h
+++ Excercise4/microTrace04/include/04/RefractiveShader.h
@@ -37,10 +37,45 @@ public:
     {
       // IMPLEMENT ME 4.1
       // (1) Calculate refractive index.
+		float n, n1 = indexInside, n2 = indexOutside;
+
       // (2) Get shading normal and look from where the ray is coming.
-      //     Modify normal and refractive index when necessary.
+      //     Modify normal and refractive index when necessary
+		bool isComingIn = false;
+		Vec3f normal = ray.hit->getNormal(ray);
+
+		if(dot(ray.dir, normal) > 0) {
+			isComingIn = true;
+			normal = -normal;
+			n1 = indexOutside;
+			n2 = indexInside;
+		}
+		n = n1/n2;
+
       // (3) Calculate the refracted ray direction.
+		float c1 = dot(-ray.dir, normal);
+		float c2_sq = 1-n*n*(1-c1*c1);
+
       // (4) Make cases for refraction and total reflection.
+		if(c2_sq < 0) {
+			Ray reflection;
+		    reflection.org = ray.org + ray.t * ray.dir;
+		    reflection.dir = ray.dir - 2*dot(normal,ray.dir)*normal;;
+		    reflection.t = INFINITY;
+		    reflection.hit = NULL;
+
+			return 0*componentProduct(color,scene->rayTrace(reflection));
+		}
+
+		ray.org = ray.org + ray.t * ray.dir;
+        ray.dir = (n * c1 - sqrt(c2_sq)) * normal + ray.dir * n;
+        ray.t = INFINITY;
+        ray.hit = NULL;
+        ray.recursionLevel++;
+
+		//result = R*tmp;
+		ret_val +=  componentProduct(color,scene->rayTrace(ray));
+
       // IMPLEMENT ME END
 
     }
